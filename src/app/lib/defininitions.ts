e37f77020a53b66c17d3f96e250f1f3ed304adc4
Deepak K V <deepudeepz383@gmail.com>
2025-01-13 22:38:03 +0530

    database table for user logs created and seeded with example dataset

---
+++ src/app/lib/defininitions.ts
@@ -0,0 +1,15 @@
+export type ftUsers = {
+    fu_id:string; 
+    fu_name:string;
+    fu_email:string; 
+    fu_phone:string; 
+    fu_password:string;
+    fu_address:string; 
+    fu_country:string; 
+    fu_state:string; 
+    fu_sex:string; 
+    fu_age:number; 
+    fu_govtIDType:string; 
+    fu_govtIDNum:string; 
+    fu_role:string;
+}
---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,20 @@
+const ftUsers = [
+    {
+        fu_id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        fu_name: 'lucid',
+        fu_email: 'lucid@futuraa.com',
+        fu_phone:'9998887774',
+        fu_password: '123456789',
+        fu_address: 'Ottapalam, Kerala',
+        fu_country: 'India',
+        fu_state: 'Kerala',
+        fu_sex: 'Male',
+        fu_age:'29',
+        fu_govtIDType:'AADHAR',
+        fu_govtIDNum:'1111222233334444',
+        fu_role:'Admin',
+    },
+  ];
+
+
+export {ftUsers};
