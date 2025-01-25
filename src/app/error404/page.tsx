c04f605018dded67e1e48244a823c68eabdd7c2e
Deepak K V <36558723+deepuvkrish@users.noreply.github.com>
2025-01-25 20:21:54 +0530

    Delete src directory

--- src/app/error404/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Error404() {
-  return (
-    <div className="flex flex-col w-full h-full justify-center items-center bg-sky-700">
-      <h1 className="text-4xl text-white">404</h1>
-      <h2 className="text-2xl text-white">Page Not Found</h2>
-    </div>
-  );
-}
--- src/app/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <div className="flex w-full h-full justify-center items-center">
-      <h1 className="text-gray-700">Welcome to futuraaa!</h1>
-    </div>
-  );
-}
